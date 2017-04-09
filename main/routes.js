dc6ab4d7b6bcc100df60f0e5d6bbca0084bd4c61
Carlin Au <carlinau@gmail.com>
2017-04-09 05:29:43 -0400

    Added templating

--- main/routes.js
+++ main/routes.js
@@ -1,5 +1,11 @@
 const init = function RoutesHandler(app) {
-  
+  app.get('/', (req, res)=>{
+    res.render('pages/main.ejs');
+  });
+
+  app.get('/login', (req, res)=>{
+    res.render('pages/login.ejs');
+  });
 }
 
 module.exports = init;
